09bfc8258fa24ed637884c155d9140e7244a17d4
Joshua Song <53218696+rlcjf0014@users.noreply.github.com>
2019-09-24 22:40:33 +0900

    Add files via upload

---
+++ mini-hackathon-pos/data.js
@@ -0,0 +1,23 @@
+var menu = [
+  {
+    id: 'americano',
+    name: '아메리카노',
+    price: 2000
+  },
+  {
+    id: 'caffelatte',
+    name: '까페라떼',
+    price: 2500
+  }
+];
+
+var cart = [
+  {
+    id: 'americano',
+    quantity: 2
+  },
+  {
+    id: 'caffelatte',
+    quantity: 1
+  }
+]
---
+++ mini-hackathon-pos/logic.js
@@ -0,0 +1,27 @@
+function createMenuElement(menu) {
+
+}
+
+function createCartItemElement(item) {
+
+}
+
+function renderMenuHTML() {
+
+}
+
+function renderCartHTML() {
+
+}
+
+function renderSubtotalHTML(arr) {
+
+}
+
+renderMenuHTML();
+
+/*
+사용자가 도착지를 입력한다.
+입력값#start에 입력갑
+단, 입력값이 알맞은 값이 아닐 시 
+*/
